c060185f88b6f8ffd151b49861048c6038c37c6d
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-03-25 10:53:18 +0100

    Add project files.

---
+++ Tomi_helyjegy_2021.03.25/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomi_helyjegy_2021._03._25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

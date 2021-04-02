c67d505f660dcff01b8459bc755bc0ea38d31e9f
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-04-02 10:17:52 +0200

    10:17

--- Tomi_helyjegy_2021.03.25/Program.cs
+++ Tomi_helyjegy_2021.03.25/Program.cs
@@ -3,13 +3,110 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Tomi_helyjegy_2021._03._25
 {
     class Program
     {
+        struct Adat
+        {
+            public int ulesszam;
+            public int kmtol;
+            public int kmig;
+            public int megtett_tavolsag;
+        }
         static void Main(string[] args)
         {
+            Adat[] adatok = new Adat[500];
+            StreamReader olvas = new StreamReader(@"C:\Users\Rendszergazda\Downloads\eladott.txt");
+            string elsosor = olvas.ReadLine();
+            string[] elsosordb = elsosor.Split();
+            int max_eladottJegy = int.Parse(elsosordb[0]);
+            int max_megtettKm = int.Parse(elsosordb[1]);
+            int max_jegyar = int.Parse(elsosordb[2]);
+            int n = 0;
+            while (!olvas.EndOfStream)
+            {
+                string sor = olvas.ReadLine();
+                string[] db = sor.Split();
+                adatok[n].ulesszam = int.Parse(db[0]);
+                adatok[n].kmtol = int.Parse(db[1]);
+                adatok[n].kmig = int.Parse(db[2]);
+                adatok[n].megtett_tavolsag = int.Parse(db[2]) - int.Parse(db[1]);
+                n++;
+            }
+            olvas.Close();
+            Console.WriteLine("1.feladat\nBeolvasás kész!");
+
+            //2.feladat
+            Console.WriteLine($"2.Feladat:\n{adatok[n-1].ulesszam} a sorszáma és {adatok[n-1].megtett_tavolsag} km-t tett meg.");
+
+            //3.Feladat
+            for (int i =0;i<n;i++)
+            {
+                if (adatok[i].megtett_tavolsag == max_megtettKm)
+                {
+                    Console.Write(adatok[i].ulesszam+" ");
+                }
+            }
+            Console.ReadKey();
         }
+
+        //függvény a megtett távolság átalakítására
+        static int megtettavolsag(int tavolsag)
+        {
+            int egesz = tavolsag / 10;
+            if (tavolsag % 10 > 0)
+            {
+                return egesz + 1;
+            }
+            else
+            {
+                return egesz;
+            }
+        }
+        static int ertek(int fizetendo)
+        {
+            int maradek = fizetendo % 10;
+            if (maradek == 1 || maradek == 2)
+            {
+                return fizetendo - maradek;
+            }
+            else if (maradek == 3 || maradek == 4)
+            {
+                return fizetendo + (5-maradek);
+            }
+            else if (maradek == 6 || maradek == 7)
+            {
+                return fizetendo - (maradek - 5);
+            }
+            else if (maradek == 8 || maradek == 9)
+            {
+                return fizetendo + (10 - maradek);
+            }
+            else
+            {
+                return fizetendo;
+            }
+           /* if (fizetendo % 10 > 0 && fizetendo % 10 <= 2)
+            {
+
+            }
+            else if (fizetendo % 10 > = 8 && fizetendo % 10 =< 9)
+            {
+
+            }
+            else if (fizetendo % 10 > = 3 && fizetendo%10<5)
+            {
+
+            }
+            else if (fizetendo % 10 > 5 )
+            {
+
+            }*/
+        }
+
+
     }
 }
